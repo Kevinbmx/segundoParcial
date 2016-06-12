958ea3c81f95b643626f9ebfd7356c5023d7de29
David <nanomaxd@gmail.com>
2016-06-12 19:43:35 -0400

    .

---
+++ database/base 1-1-5/1-actualizacion de tabla.sql
@@ -0,0 +1,78 @@
+USE [Parcialsoft2]
+GO
+ALTER TABLE dbo.tblTransaccion ADD
+	hora varchar(50) NULL
+GO
+ALTER TABLE dbo.tblTransaccion SET (LOCK_ESCALATION = TABLE)
+
+
+
+GO
+ALTER procedure [dbo].[SeleccionarTransaccion]	
+as 
+begin 
+	
+		SELECT idtransaccion, monto, fecha,idcatergoria,idcuenta,tipo,hora FROM tblTransaccion
+		
+	
+	
+		
+	end 
+go
+ALTER procedure [dbo].[InsertarTransaccion] 
+									@monto int,
+									@fecha date,
+									@idcategoria int,
+									@idcuenta int,
+									@tipo varchar(50),
+									@hora varchar(50)
+as 
+begin 
+	
+		INSERT INTO tblTransaccion (monto, fecha,idcatergoria,idcuenta,tipo,hora) VALUES (@monto,@fecha,@idcategoria,@idcuenta,@tipo,@hora)
+		
+	
+	
+		
+	end 
+
+GO
+alter procedure [dbo].[buscarTransaccionFecha] @fecha date,@idcuenta int
+as 
+begin 
+	
+		SELECT idtransaccion, monto, fecha,idcuenta,idcatergoria,tipo,hora FROM tblTransaccion  where fecha=@fecha and idcuenta=@idcuenta
+		
+end
+
+GO
+ALTER procedure [dbo].[selectbyId] @id int
+as
+begin 
+SELECT idtransaccion, monto, fecha,idcatergoria,idcuenta,tipo,hora FROM tblTransaccion WHERE idtransaccion =@id
+end 
+
+
+go
+create procedure actualizarTransaccion @id int,
+										@fecha date,
+										@hora varchar(50),
+										@tipo varchar(50),
+										@monto float,
+										@idcuenta int,
+										@idcate int
+										as
+begin
+update tblTransaccion set	fecha=@fecha,
+							hora=@hora,
+							tipo=@tipo,
+							monto=@monto,
+							idcuenta=@idcuenta,
+							idcatergoria=@idcate
+							where idtransaccion=@id
+
+							end
+
+
+
+
--- database/base 1-1-5/1-actualizaciontranasaccion.sql
+++
@@ -1,6 +0,0 @@
-GO
-ALTER TABLE dbo.tblTransaccion ADD
-	hora varchar(50) NULL
-GO
-ALTER TABLE dbo.tblTransaccion SET (LOCK_ESCALATION = TABLE)
-GO
